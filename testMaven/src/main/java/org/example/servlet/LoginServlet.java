4a5320eb278542e43dab3aa0f2cb01cdce22a633
canceru <1449515027@qq.com>
2021-03-10 22:54:01 +0800

    Maven

---
+++ testMaven/src/main/java/org/example/servlet/LoginServlet.java
@@ -0,0 +1,15 @@
+package org.example.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 14495
+ * Date: 2021-03-07
+ * Time: 17:34
+ */
+@WebServlet
+public class LoginServlet extends HttpServlet {
+}
