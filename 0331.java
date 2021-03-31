528764ed3477153632db4482ea579d456df69484
canceru <1449515027@qq.com>
2021-03-31 23:04:18 +0800

    0331

---
+++ 0331.java
@@ -0,0 +1,60 @@
+public class TestDemo {
+    /*public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m  =sc.nextInt();
+        int n = sc.nextInt();
+        int[][] array = new int[m][n];
+        for (int i = 0; i <m ; i++) {
+            for (int j = 0; j <n; j++) {
+                array[i][j]= sc.nextInt();
+            }
+        }
+        for (int i = 0; i <n ; i++) {
+            for (int k = 0, j = i; j < m && j >= 0; ) {
+                System.out.println(array[i][j]);
+                System.out.println(",");
+                k++;
+                j--;
+            }
+
+        }
+        for (int i = 0; i <m ; i++) {
+            for (int k = i, j = n - 1; j < m && j >= 0; ) {
+                System.out.println(array[i][j]);
+                k++;
+                j--;
+                if (!(i == m - 1 && j == n - 1)) {
+                    System.out.println(",");
+                }
+            }
+        }
+    }*/
+
+    /*public static void main(String[] args) {
+        System.out.println(total(15));
+    }
+    public static int total(int year) {*/
+        /*static int count = 1;
+        for (int i = 2; i <= year; i++) {
+            if (i == 2 || i == 4) {
+                count++;
+                total(year - 1);
+            }
+            if (i == 5) {
+                count--;
+                break;
+            }
+        }
+        return count;*/
+        /*int count =1;
+        for (int i = 1; i <=year ; i++) {
+            if(i==2){
+                count+=total(year-2);
+            }else if(i==4){
+                count+=total(year-4);
+            }else if(i==5){
+                count--;
+            }
+        }
+        return count;
+    }*/
