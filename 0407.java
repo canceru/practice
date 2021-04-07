fc0177a2886b6ada8f5be1e52708939cd4629ea3
canceru <1449515027@qq.com>
2021-04-07 22:51:05 +0800

    0407

---
+++ 0407.java
@@ -0,0 +1,33 @@
+哦，不！你不小心把一个长篇文章中的空格、标点都删掉了，并且大写也弄成了小写。像句子"I reset the computer. It still didn’t boot!"已经变成了"iresetthecomputeritstilldidntboot"。在处理标点符号和大小写之前，你得先把它断成词语。当然了，你有一本厚厚的词典dictionary，不过，有些词没在词典里。假设文章用sentence表示，设计一个算法，把文章断开，要求未识别的字符最少，返回未识别的字符数。
+
+class Solution {
+    public int respace(String[] dictionary, String sentence) {
+        Set<String> dict = new HashSet<>(Arrays.asList(dictionary));
+        int n = sentence.length();
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            dp[i] = dp[i - 1] + 1;
+            for (int idx = 0; idx < i; idx++) {
+                if (dict.contains(sentence.substring(idx, i))) {
+                    dp[i] = Math.min(dp[i], dp[idx]);
+                }
+            }
+        }
+        return dp[n];
+    }
+}
+
+统计字符串中的单词个数，这里的单词指的是连续的不是空格的字符。
+
+请注意，你可以假定字符串里不包括任何不可打印的字符。
+class Solution {
+    public int countSegments(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if ((i == 0 || s.charAt(i-1) == ' ') && s.charAt(i) != ' ') {
+                count++;
+            }
+        }
+        return count;
+    }
+}
