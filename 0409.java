ccbc1da39d8a04337b0a2e18ecb20b4752d3509a
canceru <1449515027@qq.com>
2021-04-09 23:45:09 +0800

    0409

---
+++ 0409.java
@@ -0,0 +1,16 @@
+给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length(), ans = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int end = 0, start = 0; end < n; end++) {
+            char alpha = s.charAt(end);
+            if (map.containsKey(alpha)) {
+                start = Math.max(map.get(alpha), start);
+            }
+            ans = Math.max(ans, end - start + 1);
+            map.put(s.charAt(end), end + 1);
+        }
+        return ans;
+    }
+}
